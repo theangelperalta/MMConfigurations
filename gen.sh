ca12c55ce3fe3123ab2cca1bc3a19c6ccea04724
Angel Cortez <acort3255@gmail.com>
2020-07-11 15:33:37 -0400

    • Improved gen.sh script for producing json configurations • added .gitignore

--- gen.sh
+++ gen.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-jsonnet --ext-str env="prod" --ext-str buildNumber="913" mm-ios-v1.jsonnet
+#set the buildNumber to the total number of commits on current branch plus a constant
+commitCount=$(git rev-list HEAD --count)
+buildNumber=$(($commitCount + 1300000))
+
+branch=$(git name-rev --name-only HEAD)
+
+if [ $branch == master ]; then
+    branch="prod"
+    jsonnet --ext-str env=$branch --ext-str buildNumber=$buildNumber mm-ios-v1.jsonnet
+elif [[ $branch == "stage" || $branch == "test" ]]; then
+    jsonnet --ext-str env=$branch --ext-str buildNumber=$buildNumber mm-ios-v1.jsonnet
+fi
